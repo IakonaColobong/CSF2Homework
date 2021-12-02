2de6f99abcef52dca3e9784d9305cc697c7b06de
Jason Colobong <jasoncolobong@outlook.com>
2021-12-01 20:41:24 -0600

    Added CreditCArdAccount info

---
+++ ClassesLibrary/CreditCardAccout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class CreditCardAccout
+    {
+
+        //Frugal / Fields  --- this is the order you generally find them in class  Files. 
+        private int _accountNumber;
+        private decimal _balance;
+        private bool _isPastDue;
+        private decimal _annualInterestRate;
+        // add after creation of customer - private Customer 
+
+        //People / Properties
+
+        //Collect / Constructors (ctors)
+
+        //Money / Methods
+
+    }
+}
